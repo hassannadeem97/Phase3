26a56af86d365fec850d3287a5e1ed6bfd3663c5
Hassan Nadeem <hassann@qatar.cmu.edu>
2018-04-09 17:57:42 +0000

    Done with all the html and css stuff, site design completed

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+    def index
+        render template: "pages/#{params[:page]}"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   resources :camp_instructors
   resources :camps
   resources :curriculums
   resources :locations
   resources :instructors
+  get "/pages/:page" => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
